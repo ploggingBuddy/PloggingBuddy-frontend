2e0531760224c2a6ad9d5d8860ae02df41405454
DeveloperJongseok <developerjongseok2024@gmail.com>
2025-05-19 22:04:32 +0900

    get member Info

--- src/pages/ProfileInfo.jsx
+++ src/pages/ProfileInfo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import profileDefault from "../assets/profile_default.jpg";
 import ProfileField from "../components/ProfileField";
 
