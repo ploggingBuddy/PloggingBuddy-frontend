cd8852601b35b12f008a46cc254e37de2f44cb5e
DeveloperJongseok <developerjongseok2024@gmail.com>
2025-05-22 03:33:42 +0900

    data format

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,7 +1,7 @@
 export const formatDate = (dateString) => {
   const date = new Date(dateString);
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
   return `${month}/${day}`;
 };
 
@@ -17,9 +17,9 @@ export const formatAddress = (address) => {
 
 export const formatDateTime = (dateTimeString) => {
   const date = new Date(dateTimeString);
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
   return `${month}월 ${day}일 ${hours}시 ${minutes}분`;
 };
